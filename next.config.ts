7e49d12a07c61ff66fc5bfcd43ec36dec2721170
rvph10 <rgenu10@gmail.com>
2025-05-13 23:29:31 +0200

    chore(nextjs): configuration file updated

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["lowlink.app"],
+    domains: ["lowlink.app", "kodmubndfoybzuzdjghn.supabase.co"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lowlink.app",
+      },
+      {
+        protocol: "https",
+        hostname: "kodmubndfoybzuzdjghn.supabase.co",
+        pathname: "/storage/v1/object/public/profile-pictures/**",
       },
     ],
   },
